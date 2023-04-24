ca9bb1faaf1f61895a9c7c19c9a02bcd1e6a489c
Lê Danh Trọng <trongld@nexusti.vn>
2023-04-24 20:28:56 +0700

    done profile, fixbug

---
+++ lib/store/user/user_store.g.dart
@@ -0,0 +1,58 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$UserStore on _UserStore, Store {
+  late final _$isLoadingAtom =
+      Atom(name: '_UserStore.isLoading', context: context);
+
+  @override
+  bool get isLoading {
+    _$isLoadingAtom.reportRead();
+    return super.isLoading;
+  }
+
+  @override
+  set isLoading(bool value) {
+    _$isLoadingAtom.reportWrite(value, super.isLoading, () {
+      super.isLoading = value;
+    });
+  }
+
+  late final _$messageAtom = Atom(name: '_UserStore.message', context: context);
+
+  @override
+  String? get message {
+    _$messageAtom.reportRead();
+    return super.message;
+  }
+
+  @override
+  set message(String? value) {
+    _$messageAtom.reportWrite(value, super.message, () {
+      super.message = value;
+    });
+  }
+
+  late final _$updateProfileAsyncAction =
+      AsyncAction('_UserStore.updateProfile', context: context);
+
+  @override
+  Future<bool> updateProfile(UserModel newUser) {
+    return _$updateProfileAsyncAction.run(() => super.updateProfile(newUser));
+  }
+
+  @override
+  String toString() {
+    return '''
+isLoading: ${isLoading},
+message: ${message}
+    ''';
+  }
+}
